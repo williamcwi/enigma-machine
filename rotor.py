dc7740c40255f673fdd1a008bcaf14d436bd7633
williamcwi <williamcwi.chu@hotmail.com>
2022-04-21 20:05:23 +0100

    added rotor and reflector wirings

---
+++ rotor.py
@@ -0,0 +1,34 @@
+rotors = {
+    'I': {
+        'wiring': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+        'turnover': 'Q' # If rotor steps from Q to R, the next rotor is advanced
+    }, 
+    'II': {
+        'wiring': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+        'turnover': 'E' # If rotor steps from E to F, the next rotor is advanced
+    }, 
+    'II': {
+        'wiring': 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+        'turnover': 'V' # 	If rotor steps from V to W, the next rotor is advanced
+    }, 
+    'IV': {
+        'wiring': 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+        'turnover': 'J' # If rotor steps from J to K, the next rotor is advanced
+    }, 
+    'V': {
+        'wiring': 'VZBRGITYUPSDNHLXAWMJQOFECK',
+        'turnover': 'Z' # If rotor steps from Z to A, the next rotor is advanced
+    }, 
+    'VI': {
+        'wiring': 'JPGVOUMFYQBENHZRDKASXLICTW',
+        'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
+    }, 
+    'VII': {
+        'wiring': 'NZJHGRCXMYSWBOUFAIVLPEKQDT',
+        'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
+    }, 
+    'VIII': {
+        'wiring': 'FKQHTLXOCBJSPDZRAMEWNIUYGV',
+        'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
+    }
+}
