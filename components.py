9155f88f8b4d20f8ffc380f423b61c35aeb0a606
williamcwi <williamcwi.chu@hotmail.com>
2022-04-22 01:07:12 +0100

    added rotor inverse wiring

--- components.py
+++ components.py
@@ -1,34 +1,44 @@
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 rotors = {
     'I': {
         'wiring': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+        # 'inverse': 'UWYGADFPVZBECKMTHXSLRINQOJ',
         'turnover': 'Q' # If rotor steps from Q to R, the next rotor is advanced
     }, 
     'II': {
         'wiring': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+        # 'inverse': 'AJPCZWRLFBDKOTYUQGENHXMIVS',
         'turnover': 'E' # If rotor steps from E to F, the next rotor is advanced
     }, 
     'III': {
         'wiring': 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+        # 'inverse': 'TAGBPCSDQEUFVNZHYIXJWLRKOM',
         'turnover': 'V' # 	If rotor steps from V to W, the next rotor is advanced
     }, 
     'IV': {
         'wiring': 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+        # 'inverse': 'HZWVARTNLGUPXQCEJMBSKDYOIF',
         'turnover': 'J' # If rotor steps from J to K, the next rotor is advanced
     }, 
     'V': {
         'wiring': 'VZBRGITYUPSDNHLXAWMJQOFECK',
+        # 'inverse': 'QCYLXWENFTZOSMVJUDKGIARPHB',
         'turnover': 'Z' # If rotor steps from Z to A, the next rotor is advanced
     }, 
     'VI': {
         'wiring': 'JPGVOUMFYQBENHZRDKASXLICTW',
+        # 'inverse': '',
         'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
     }, 
     'VII': {
         'wiring': 'NZJHGRCXMYSWBOUFAIVLPEKQDT',
+        # 'inverse': '',
         'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
     }, 
     'VIII': {
         'wiring': 'FKQHTLXOCBJSPDZRAMEWNIUYGV',
+        # 'inverse': '',
         'turnover': 'ZM' # 	If rotor steps from Z to A, or from M to N the next rotor is advanced
     }
 }
@@ -55,6 +65,13 @@ class Rotor():
         self.starting_position = starting_position
         self.next_forward = next_forward
         self.next_backward = next_backward
+
+        self.forward = {}
+        self.backward = {}
+
+        for start, end in zip(alphabet, self.wiring):
+            self.forward[start] = end
+            self.backward[end] = start
 
 class Reflector():
     def __init__(self, reflector_model):
